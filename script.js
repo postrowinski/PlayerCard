e0b96986eed49fabaed6a1d6b6d5e3e62373599f
Przemek <p.ostrowinski@gmail.com>
2017-07-09 23:47:15 +0200

    remove jquery

--- script.js
+++ script.js
@@ -1,66 +1,81 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function () {
     'use strict';
     
+    var statsField = document.querySelectorAll('.number-place'),
+        minusBtn = document.querySelectorAll('.minus'),
+        plusBtn = document.querySelectorAll('.plus'),
+        randomizeBtn = document.querySelector('#randomize'),
+        pointPoolField = document.querySelector('#point-pool');
+
     //function randomize value between 3 and 18
     function randomize() {
         var randomNumber = Math.floor(Math.random() * 15 + 3);
         return randomNumber;
     }
     
-    //function set random value for each abilities
-    function setRandom() {
-        $('.number-place').each(function () {
-            $(this).text(randomize());
-        });
+    //function set random value for each abilities   
+    function setRandom() {     
+        for (let i = 0; i < statsField.length; i++) {
+            let statsVal = document.createTextNode(randomize());
+    
+            statsField[i].innerHTML = '';
+            statsField[i].appendChild(statsVal);
+        }
     }
     
     //Subtraction choose stats 
     //&& Adding to main pool
-    $('.minus').click(function () {
-        var poolPoints = $('#point-pool').text(),
-            poolPointstoNumber = Number(poolPoints),
-            getID = $(this).parent().attr('id'),
-            getRightNumberPlace = $('#'+ getID).find('.number-place'),
-            getText = $(getRightNumberPlace).text(),
-            setNumber = Number(getText);
-        setNumber--;
-        poolPointstoNumber++;
+    Array.from(minusBtn).forEach(function(element) {
+        element.addEventListener('click', function() {
+            var poolPoints = document.querySelector('#point-pool').innerHTML,
+                poolPointstoNumber = Number(poolPoints),
+                getID = this.parentNode.getAttribute('id'),
+                getRightNumberPlace = document.querySelector(`#${getID} .number-place`),
+                getText = getRightNumberPlace.innerHTML,
+                setNumber = Number(getText);
+            
+             setNumber--;
+             poolPointstoNumber++;
         
-        if (setNumber < 3 || poolPointstoNumber === -1) {
-            return false;
-        } else {
-            $(getRightNumberPlace).text(setNumber);
-            $('#point-pool').text(poolPointstoNumber);
-        } 
+            if (setNumber < 3 || poolPointstoNumber === -1) {
+                return false;
+            } else {
+                getRightNumberPlace.innerHTML = setNumber;
+                document.querySelector('#point-pool').innerHTML = poolPointstoNumber;
+            } 
+        });                         
     });
-    
+  
     //Adding choose stats 
     //&& Subtractioning from main pool
-    $('.plus').click(function() {
-        var poolPoints = $('#point-pool').text(),
-            poolPointstoNumber = Number(poolPoints),
-            getID = $(this).parent().attr('id'),
-            getRightNumberPlace = $('#' + getID).find('.number-place'),
-            getText = $(getRightNumberPlace).text(),
-            setNumber = Number(getText);
-        setNumber++;
-        poolPointstoNumber--;
-        
-        if (setNumber > 18 || poolPointstoNumber < 0) {
-            return false;
-        } else {
-            $(getRightNumberPlace).text(setNumber);
-            $('#point-pool').text(poolPointstoNumber);
-        } 
+    Array.from(plusBtn).forEach(function(element) {
+        element.addEventListener('click', function() {
+            var poolPoints = document.querySelector('#point-pool').innerHTML,
+                poolPointstoNumber = Number(poolPoints),
+                getID = this.parentNode.getAttribute('id'),
+                getRightNumberPlace = document.querySelector(`#${getID} .number-place`),
+                getText = getRightNumberPlace.innerHTML,
+                setNumber = Number(getText);
+            
+             setNumber++;
+             poolPointstoNumber--;
+            
+            if (setNumber > 18 || poolPointstoNumber < 0) {
+                return false;
+            } else {
+                getRightNumberPlace.innerHTML = setNumber;
+                document.querySelector('#point-pool').innerHTML = poolPointstoNumber;
+            } 
+        });
     });
-    
+       
     //Randomize start abilities value web webpage is loaded
     setRandom();
     
     //Click on Reroll button set new randomize value fo each stats
-    //&& reset main abilities point
-    $('#randomize').click(function () {
-        $('#point-pool').text('0');
+    //&& reset main abilities point   
+    randomizeBtn.addEventListener('click', function() {
+        pointPoolField.innerHTML = 0;
         setRandom();
-    });    
+    });
 });
